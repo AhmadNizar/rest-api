d3db0af716ddd095f06201fa2c17beb1969d976f
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-11-13 22:19:30 +0700

    coba terus ya allah

--- app.js
+++ app.js
@@ -8,6 +8,4 @@ const users = require('./routes/usersRouter')
 app.use('/api', users)
 // app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => {
-	console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || '3000')
